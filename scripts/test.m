17664ea6f53f5c09ec5aad8fc70edd766a422b92
davicn <davi.nascimento777@gmail.com>
2022-06-19 22:13:26 -0300

    iniciando função para features

--- scripts/test.m
+++ scripts/test.m
@@ -1,15 +1,26 @@
-path = 'data\raw\Data_F_Ind0001.txt';
+file = "data\partitions\Data_N_Ind3750\second=20\part.0.parquet";
 
-fs = 512;
+T = parquetread(file);
 
-opts = detectImportOptions(path);
-opts.VariableNames = {'x','y'};
-opts.Delimiter = {','};
+var_x = var(T.x);
+kurtosis_x = kurtosis(T.x);
+skewness_x = skewness(T.x);
+mean_x = mean(T.x);
+meanfreq_x = meanfreq(T.x);
+entropy_log_energy_x = wentropy(T.x,'log energy');
+entropy_shannon_x = wentropy(T.x,'shannon');
 
-seconds_ = linspace(1,20,20);
-labels = repelem(seconds_, 512);
+var_y = var(T.y);
+kurtosis_y = kurtosis(T.y);
+skewness_y = skewness(T.y);
+mean_y = mean(T.y);
+meanfreq_y = meanfreq(T.y);
+entropy_log_energy_y = wentropy(T.y,'log energy');
+entropy_shannon_y = wentropy(T.y,'shannon');
 
-t = readtable(path, opts);
-t.seconds = labels;
 
 
+
+data = table(var_x,kurtosis_x,skewness_x,mean_x,meanfreq_x,entropy_log_energy_x,entropy_shannon_x,var_y,kurtosis_y,skewness_y,mean_y,meanfreq_y,entropy_log_energy_y,entropy_shannon_y);
+
+parquetwrite('data\processed\Data_N_Ind3750\second=20.parquet',data)
